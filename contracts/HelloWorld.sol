9eea061eb1f35706eeea2cb5c2d5243c2771a246
dollarkillerx <adapawang@gmail.com>
2022-08-13 11:08:37 +0800

    init project

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,8 +1,12 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract HelloWorld {
     string public yourName;
-    constructor() public {
+
+    /* Constructor */
+    constructor() {
         yourName = "Unknown";
     }
 
